ba73a28907b879695193260f1c307ce5d13bcf74
Ghaus Ali Khan <ghauskhan1616@gmail.com>
2023-06-27 01:17:52 +0500

    Add main.js file and Add menu button functionality

---
+++ main.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector(".menu");
+const openButton = document.querySelector(".hamb-icon");
+const closeButton = document.querySelector(".close");
+
+openButton.addEventListener("click", () => {
+  nav.classList.add("visible");
+});
+
+closeButton.addEventListener("click", () => {
+  nav.classList.remove("visible");
+});
