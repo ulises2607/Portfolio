6c0dbd8594e420cf92690dcc4c5a8d2f0df4a427
ulises2607 <ulisescappa@gmail.com>
2023-06-27 12:01:31 -0300

    Adding styles to the mobile menu

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const nav = document.querySelector(".menu");
-const openButton = document.querySelector(".hamb-icon");
-const closeButton = document.querySelector(".close");
+const nav = document.querySelector('.menu');
+const openButton = document.querySelector('.hamb-icon');
+const closeButton = document.querySelector('.close');
 
-openButton.addEventListener("click", () => {
-  nav.classList.add("visible");
+openButton.addEventListener('click', () => {
+  nav.classList.add('visible');
 });
 
-closeButton.addEventListener("click", () => {
-  nav.classList.remove("visible");
+closeButton.addEventListener('click', () => {
+  nav.classList.remove('visible');
 });
