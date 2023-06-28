fb855d9dff8ee93cec3c0cfc07f3a0084934734a
ulises2607 <ulisescappa@gmail.com>
2023-06-28 16:29:32 -0300

    Adding works.js file to build the recent work section and store the data of the cards in a project array

---
+++ Scripts/works.js
@@ -0,0 +1,130 @@
+const works = document.querySelector('.recent-works');
+// console.log("Children de main", main.children[0]);
+
+const divMultPost = document.createElement('div');
+divMultPost.className = 'container';
+works.appendChild(divMultPost);
+
+const projects = [
+  {
+    id: 0,
+    name: 'Multi-Post Stories',
+    description: `A daily selection of privately personalized reads; no accounts or
+    sign-ups required. has been the industry's standard dummy text ever
+    since the 1500s, when an unknown printer took a standard dummy text.`,
+    image: 'Images/Work/Work-img-1.png',
+    technology: ['CSS', 'HTML', 'Bootstrap', 'Ruby'],
+    source: '',
+    live: '',
+  },
+  {
+    id: 1,
+    name: 'Profesional Art Printing Data 1',
+    description: `A daily selection of privately personalized reads; no accounts or
+    sign-ups required. Has been the industry's standard.`,
+    image: '',
+    technology: ['HTML', 'CSS', 'JavaScript'],
+  },
+  {
+    id: 2,
+    name: 'Profesional Art Printing Data 2',
+    description: `A daily selection of privately personalized reads; no accounts or
+    sign-ups required. Has been the industry's standard.`,
+    image: '',
+    technology: ['HTML', 'CSS', 'JavaScript', 'Ruby'],
+  },
+  {
+    id: 3,
+    name: 'Profesional Art Printing Data 3',
+    description: `A daily selection of privately personalized reads; no accounts or
+    sign-ups required. Has been the industry's standard.`,
+    image: '',
+    technology: ['HTML', 'CSS', 'JavaScript'],
+  },
+  {
+    id: 4,
+    name: 'Profesional Art Printing Data 4',
+    description: `A daily selection of privately personalized reads; no accounts or
+    sign-ups required. Has been the industry's standard.`,
+    image: '',
+    technology: ['HTML', 'CSS', 'JavaScript'],
+  },
+  {
+    id: 5,
+    name: 'Profesional Art Printing Data 5',
+    description: `A daily selection of privately personalized reads; no accounts or
+    sign-ups required. Has been the industry's standard.`,
+    image: '',
+    technology: ['HTML', 'CSS3', 'JavaScript'],
+  },
+  {
+    id: 6,
+    name: 'Profesional Art Printing Data 6 ',
+    description: `A daily selection of privately personalized reads; no accounts or
+    sign-ups required. Has been the industry's standard.`,
+    image: '',
+    technology: ['HTML', 'CSS', 'JavaScript'],
+  },
+  {
+    id: 7,
+    name: 'Profesional Art Printing Data 7 ',
+    description: `A daily selection of privately personalized reads; no accounts or
+    sign-ups required. Has been the industry's standard.`,
+    image: '',
+    technology: ['HTML', 'CSS', 'JavaScript'],
+  },
+];
+
+let techListMP = '';
+
+projects[0].technology.forEach((tech) => {
+  techListMP += `<li class="tech-btn"><a class="a-tech" href="#">${tech}</a></li>`;
+});
+
+divMultPost.innerHTML = `<img
+class="img-work"
+src="${projects[0].image}"
+alt="Multi-post-stories"
+/>
+
+<div class="mult-post">
+<h3>${projects[0].name}</h3>
+
+<p>${projects[0].description}</p>
+
+<ul class="tech">
+    ${techListMP}
+</ul>
+
+
+<a href="#" class="btn-works-1">See Projects</a>
+</div>`;
+
+const divCards = document.createElement('div');
+divCards.classList.add('card-container');
+works.appendChild(divCards);
+
+let techLang = '';
+
+for(let i = 1; i < projects.length; i++) {
+  techLang = '';
+    projects[i].technology.forEach((tech) => {
+    techLang += `<li>${tech}</li>`;
+  });
+}
+
+// eslint-disable-next-line no-plusplus
+for (let e = 1; e < projects.length; e++) {
+  divCards.innerHTML += `
+    <div class="card card-${projects[e].id}">
+        <h4 class="card-title">${projects[e].name}</h4>
+        <article class="card-description">
+          ${projects[e].description}
+        </article>
+        <ul class="card-lang">
+            ${techLang}
+        </ul>
+        <a href="#" class="btn-works">See Project</a>
+      </div>
+    `;
+}
